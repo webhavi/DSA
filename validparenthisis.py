9d7e57bfae0a04db29a16fdeb2929195c99bf0f7
vaibhavi <viiwhat081@gmail.com>
2026-06-22 23:01:28 +0530

    Add files via upload

---
+++ validparenthisis.py
@@ -0,0 +1,19 @@
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+       stack = []
+       closeToopen = {
+              ')':'(',
+              '}':'{',
+              ']':'['
+       } 
+       for c in s :
+           if c in closeToopen :
+               if stack and stack[-1] == closeToopen[c] :
+                   stack.pop()
+               else:
+                   return False
+           else:
+               stack.appened(c)
+               return not stack
