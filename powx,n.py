5faadfe1fd178772e29d8b9ab8f1a491ce92d803
webhavi <webhavi@users.noreply.github.com>
2026-06-29 10:59:19 +0530

    Add powx,n.py - Power of x solution using recursion

---
+++ powx,n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if n == 0:
+                return 1
+
+            half = helper(x, n // 2)
+
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        return helper(x, n)
