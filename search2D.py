e9c36d85ba6fd46669c8555521be493c5a6d5f83
webhavi <webhavi@users.noreply.github.com>
2026-07-02 17:29:04 +0530

    Add search2D.py - Search 2D Matrix solution

---
+++ search2D.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == target:
+                    return True
+        return False
